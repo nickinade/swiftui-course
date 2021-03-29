fadce75a1ac577c848443d549a7b0cf25c4e69be
Nikita Benin <nikitab22@me.com>
2021-03-29 11:04:29 +0300

    Base project

---
+++ SwiftUICourse/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUICourse
+//
+//  Created by Nikita Benin on 25.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
